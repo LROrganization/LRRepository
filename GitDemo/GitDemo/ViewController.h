f6b160e61ffbb9c55e01d31dbe3088cf84cd2fa7
刘蕊 <1348045707@qq.com>
2016-03-12 15:59:23 -0800

    LRTest
    6666

---
+++ GitDemo/GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 LR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/GitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 LR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *str = @"liurui";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
